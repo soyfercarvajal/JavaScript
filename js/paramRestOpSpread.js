6e5fe68742435c9a267c0389be9e178c0ad371b8
soyfercarvajal <soyfercarvajal@gmail.com>
2022-12-14 23:58:30 -0600

    Clase de parametro rest y operador spread

---
+++ js/paramRestOpSpread.js
@@ -0,0 +1,23 @@
+//Paramentros REST
+
+function sumar(a, b, ...c) {
+  let resultado = a + b;
+  c.forEach(function (n) {
+    resultado += n;
+  });
+  return resultado;
+}
+
+console.log(sumar(1, 2));
+console.log(sumar(1, 2, 3));
+console.log(sumar(1, 2, 3, 4));
+console.log(sumar(1, 2, 3, 4, 5));
+
+//Operador Spread
+const arr1 = [1, 2, 3, 4, 5];
+arr2 = [6, 7, 8, 9, 0];
+
+console.log(arr1, arr2);
+
+const arr3 = [...arr1, ...arr2];
+console.log(arr3);
