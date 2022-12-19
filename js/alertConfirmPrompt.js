a341917b3edc78417d632d5e6e68c580c2aeeda9
soyfercarvajal <soyfercarvajal@gmail.com>
2022-12-18 23:38:02 -0600

    Clase de alert confirm y prompt

---
+++ js/alertConfirmPrompt.js
@@ -0,0 +1,14 @@
+// console.log(window);
+// alert("Hola esto es una alerta");
+// confirm("Hola esto es una confirmación");
+// prompt("Hola esto es un aviso y le permite al usuario ingresar un valor.");
+
+let alerta = alert("Hola esto es una alerta"),
+  confirmacion = confirm("Hola esto es una confirmación"),
+  aviso = prompt(
+    "Hola esto es un aviso y le permite al usuario ingresar un valor."
+  );
+
+console.log(alerta);
+console.log(confirmacion);
+console.log(aviso);
